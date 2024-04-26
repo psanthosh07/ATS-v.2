3f21a2a86dec9286c4437dfeba770d208ced8ee1
Santhosh P <58479203+psanthosh07@users.noreply.github.com>
2024-04-26 22:19:20 +0530

    Add files via upload

---
+++ fb.py
@@ -0,0 +1,23 @@
+import firebase_admin
+from firebase_admin import credentials, storage
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate("D:\ats100-e78c6-firebase-adminsdk-klyof-f2f1e0b08a.json")
+firebase_admin.initialize_app(cred, {
+    'storageBucket': 'your-storage-bucket-url'
+})
+
+# Initialize Firebase Storage
+bucket = storage.bucket()
+
+# Upload a text file
+def upload_file(file_path, destination_blob_name):
+    blob = bucket.blob(destination_blob_name)
+    blob.upload_from_filename(file_path)
+    print("File uploaded to Firebase Storage.")
+
+# Example usage
+if __name__ == "__main__":
+    file_path = "path/to/your/text_file.txt"
+    destination_blob_name = "uploaded_file.txt"
+    upload_file(file_path, destination_blob_name)
